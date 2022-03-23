33ea6e829dcd71c693467235e1b08195a397e028
Ken Prehoda <kenprehoda@gmail.com>
2022-03-23 08:55:44 -0700

    initial commit

---
+++ Lecture 1/lec_1.py
@@ -0,0 +1,15 @@
+# Lecture 1- Running python in VSCode
+# Play, cells, notebooks
+
+# VSCode - editor; Anaconda - python distribution
+
+# kernel
+# %%
+2+2
+print(2+2)
+
+# %%
+
+
+def atEquilibrium(q: float, dgnaught: float) -> float:
+    pass
